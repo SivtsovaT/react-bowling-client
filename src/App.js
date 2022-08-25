96771d612c9f646a74f49dc88b080cac2ec08c4b
user <sivtsovatanya1985@gmail.com>
2022-08-25 15:50:13 +0300

    implementation of data transfer in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import GenderPage from "./components/gender-page/GenderPage";
 import NamePage from "./components/name-page/NamePage";
 
 const App = () => {
+    const [user, setUser] = useState({
+        userName: '',
+        userGender: '',
+        userBirthday: ''
+    })
+    const updateData = (v1, v2, v3) => {
+        setUser({
+            userName: v1,
+            userGender: v2,
+            userBirthday: v3
+
+        })
+    }
     return (
         <div>
-           <NamePage/>
+           <NamePage updateData={updateData}/>
             {/*<GenderPage/>*/}
         </div>
     )
 }
 
 export default App;
+
+
+// state = {
+//     name: 'Бумеранг не запущен'
+// };
+//
+// updateData = (value) => {
+//     this.setState({ name: value })
+// }
+//
+// render() {
+//     return (
+//         <div>
+//             <p>State: {this.state.name}</p>
+//             <ChildComponent updateData={this.updateData} />
+//         </div>
+//     )
+// }
+// }
--- src/components/name-page/NamePage.js
+++ src/components/name-page/NamePage.js
@@ -1,16 +1,22 @@
 import React, {useState} from "react";
 import './NamePage.css';
+import {value} from "firebase-tools/lib/deploymentTool";
 
-const NamePage = () => {
+const NamePage = (props) => {
 	const [userName, setUserName] = useState('');
-	const [value, setValue] = useState('male');
+	const [gender, setGender] = useState('male');
 	const [date, setDate] = useState('');
+	const [user, setUser] = useState({
+		userName: userName,
+		userGender: gender,
+		userBirthday: date
+	})
 
 	const handleChange = (event) => {
 		setUserName(event.target.value);
 	}
 	const handleChooseGender = (event) => {
-		setValue(event.target.value);
+		setGender(event.target.value);
 	}
 	const handleChooseBirthday = (event) => {
 		setDate(event.target.value);
@@ -19,11 +25,24 @@ const NamePage = () => {
 	const handleSubmit  = () => {
 		let user = {
 			userName: userName,
-			userGender: value,
+			userGender: gender,
 			userBirthday: date
 		}
 		console.log(user);
 	}
+// 	state = {
+// 		name: 'Бумеранг вернулся назад'
+// 	}
+//
+// 	render() {
+// 		return (
+// 			<div>
+// 				<button onClick={() => { this.props.updateData(this.state.name)}}>Запустить бумеранг</button>
+// 			</div>
+// 		)
+// 	}
+// }
+
 
 	return (
 		<div className='container'>
@@ -43,22 +62,21 @@ const NamePage = () => {
 					<input type='radio'
 						   name='radio'
 						   value='male'
-						   checked={value === 'male' ? true : false}
+						   checked={gender === 'male' ? true : false}
 						   onChange={handleChooseGender}/>
 
 					<input type='radio'
 						   name='radio'
 						   value='female'
-						   checked={value === 'female' ? true : false}
+						   checked={gender === 'female' ? true : false}
 						   onChange={handleChooseGender}/>
-					<p>{value}</p>
+					<p>{gender}</p>
 					<input type='date'
 						   name='date'
 						   value={date}
 						   onChange={handleChooseBirthday}/>
 				</div>
-
-
+				<button onClick={() => {props.updateData(userName, gender, date)}}>SAVE</button>
 				<button onClick={handleSubmit} className='w-90 accept-button'>NEXT</button>
 			</div>
 		</div>
