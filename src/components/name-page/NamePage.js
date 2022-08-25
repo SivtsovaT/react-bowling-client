2564ef1ce2eedeae5c6ed83380f00094881dc68b
user <sivtsovatanya1985@gmail.com>
2022-08-25 14:35:44 +0300

    added gender & birthday

--- src/components/name-page/NamePage.js
+++ src/components/name-page/NamePage.js
@@ -3,15 +3,28 @@ import './NamePage.css';
 
 const NamePage = () => {
 	const [userName, setUserName] = useState('');
+	const [value, setValue] = useState('male');
+	const [date, setDate] = useState('');
+
 	const handleChange = (event) => {
 		setUserName(event.target.value);
 	}
-	const handleSubmit  = () => {
-		let users = {
-			userName: userName,
-		}
-		console.log(users);
+	const handleChooseGender = (event) => {
+		setValue(event.target.value);
 	}
+	const handleChooseBirthday = (event) => {
+		setDate(event.target.value);
+	}
+
+	const handleSubmit  = () => {
+		let user = {
+			userName: userName,
+			userGender: value,
+			userBirthday: date
+		}
+		console.log(user);
+	}
+
 	return (
 		<div className='container'>
 			<div className='header'>
@@ -19,13 +32,33 @@ const NamePage = () => {
 			</div>
 			<div className='content'>
 				<h2>What is your name?</h2>
-				<input
-					className='w-90'
-					type='text'
-					placeholder='enter name'
-					value={userName}
-				    onChange={handleChange}
-				/>
+					<input
+						className='w-90'
+						type='text'
+						placeholder='enter name'
+						value={userName}
+						onChange={handleChange}
+					/>
+				<div>
+					<input type='radio'
+						   name='radio'
+						   value='male'
+						   checked={value === 'male' ? true : false}
+						   onChange={handleChooseGender}/>
+
+					<input type='radio'
+						   name='radio'
+						   value='female'
+						   checked={value === 'female' ? true : false}
+						   onChange={handleChooseGender}/>
+					<p>{value}</p>
+					<input type='date'
+						   name='date'
+						   value={date}
+						   onChange={handleChooseBirthday}/>
+				</div>
+
+
 				<button onClick={handleSubmit} className='w-90 accept-button'>NEXT</button>
 			</div>
 		</div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,3 +12,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
